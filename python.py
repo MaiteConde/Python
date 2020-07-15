476894b7f5121d3dea2bcc4473e32da9d2271556
Maite <maitecondepalacio@gmail.com>
2020-07-15 21:55:26 +0200

    Python things

--- python.py
+++ python.py
@@ -1,3 +1,46 @@
 D = {"A": 10, "B": -19, "C": "abc"}
 for x in D:
     print(x,D[x])
+
+a = int(input("Write a numbre"))
+b = int(input("Write a number"))
+if b>a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+
+a = int(input("Write a number a"))
+b = int(input("Write a number b"))
+if b>a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+L = []
+for i in range(10):
+    print(i+1)
+    L.append(i**2)
+print(L)
+
+S = {"apple", 3.5, "cherry"}
+for x in S:
+    print(x)
+else:
+    print("loop ended")
+print ("out of the loop")
+
+def printOk():
+    print("Done")
+    print("Nice!")
+printOk()
+
+def checkIfNoNumeric(*args):
+    for x in args:
+        if not(isinstance(x,(int,float))):
+            return False
+    return True
+def addAllNumerics(*args):
+    s = 0
+    for x in args:
+        s+=x
+    return s
+myName = "Mai"
